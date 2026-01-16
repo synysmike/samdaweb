2f5997670893c27cb78d7053db8cb71d1369d72c
Febry San <97709170+f3brysan@users.noreply.github.com>
2026-01-16 22:41:21 +0700

    Add Laravel Sanctum and Spatie Permission integration
    - Updated composer.json to include laravel/sanctum and spatie/laravel-permission packages.
    - Created Permission and Role models extending Spatie's models with UUID as primary key.
    - Modified User model to use HasApiTokens and HasRoles traits, and set UUID as primary key.
    - Updated database migrations to create users table with UUID and added migration for permission tables.
    - Adjusted application configuration for timezone and locale settings.
    - Added permission configuration file for managing roles and permissions.

--- config/app.php
+++ config/app.php
@@ -64,8 +64,7 @@ return [
     | is set to "UTC" by default as it is suitable for most use cases.
     |
     */
-
-    'timezone' => env('APP_TIMEZONE', 'UTC'),
+    'timezone' => env('APP_TIMEZONE', 'Asia/Jakarta'),
 
     /*
     |--------------------------------------------------------------------------
@@ -78,11 +77,11 @@ return [
     |
     */
 
-    'locale' => env('APP_LOCALE', 'en'),
+    'locale' => env('APP_LOCALE', 'id'),
 
-    'fallback_locale' => env('APP_FALLBACK_LOCALE', 'en'),
+    'fallback_locale' => env('APP_FALLBACK_LOCALE', 'id'),
 
-    'faker_locale' => env('APP_FAKER_LOCALE', 'en_US'),
+    'faker_locale' => env('APP_FAKER_LOCALE', 'id_ID'),
 
     /*
     |--------------------------------------------------------------------------
