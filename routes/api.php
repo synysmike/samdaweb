1f2464717f6d5a1c1035b8e9ab2b3c1c2474254c
Febry San <97709170+f3brysan@users.noreply.github.com>
2026-01-16 23:45:56 +0700

    Refactor API and web routes for clarity and functionality
    - Removed the commented-out user retrieval route from routes/api.php to streamline the API structure.
    - Uncommented and restored the default web route in routes/web.php to ensure the welcome view is accessible.

--- routes/api.php
+++ routes/api.php
@@ -4,10 +4,6 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\AuthController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-
 Route::group(['prefix' => 'v1', 'as' => 'api.'], function () {
     Route::group(['prefix' => 'auth', 'as' => 'auth.'], function () {
         Route::post('login', [AuthController::class, 'login'])->name('login');
