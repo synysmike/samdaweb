840e36665fd75d421d89f2fa7434b67ed8c204bf
Febry San <97709170+f3brysan@users.noreply.github.com>
2026-01-26 23:23:35 +0700

    Refactor API routes for improved organization and clarity
    - Added comments to distinguish between public and protected routes, enhancing code readability.
    - Consolidated middleware usage for protected routes, streamlining the route definitions for better maintainability.
    - Ensured consistent naming conventions across route prefixes and groups, improving overall structure.

--- routes/api.php
+++ routes/api.php
@@ -12,6 +12,7 @@ use App\Http\Controllers\API\ProductCategoryController;
 use App\Http\Controllers\API\ProductSubCategoryController;
 
 Route::group(['prefix' => 'v1', 'as' => 'api.'], function () {
+    // Public routes
     Route::group(['prefix' => 'auth', 'as' => 'auth.'], function () {
         Route::post('login', [AuthController::class, 'login'])->name('login');
         Route::post('register', [AuthController::class, 'register'])->name('register');
@@ -20,29 +21,27 @@ Route::group(['prefix' => 'v1', 'as' => 'api.'], function () {
         Route::post('reset-password', [AuthController::class, 'resetPassword'])->name('reset-password');
     });
 
-
-    Route::middleware('auth:sanctum')->group(function () {
-        Route::group(['prefix' => 'world', 'as' => 'world.'], function () {
-            Route::get('countries', [WorldController::class, 'countries'])->name('countries.get');
-            Route::post('states', [WorldController::class, 'states'])->name('states.post');
-            Route::post('cities', [WorldController::class, 'cities'])->name('cities.post');
-        });
-    });
-
+    // Protected routes
     Route::middleware('auth:sanctum')->group(function () {
         Route::prefix('master')->name('master.')->group(function () {
             Route::get('membership-plans', [MasterPlanMembership::class, 'index'])->name('membership-plans.index');
         });
 
-        Route::middleware('auth:sanctum')->prefix('settings')->name('settings.')->group(function () {
+        Route::prefix('world')->name('world.')->group(function () {
+            Route::get('countries', [WorldController::class, 'countries'])->name('countries.get');
+            Route::post('states', [WorldController::class, 'states'])->name('states.post');
+            Route::post('cities', [WorldController::class, 'cities'])->name('cities.post');
+        });
+
+        Route::prefix('settings')->name('settings.')->group(function () {
             Route::get('get-profile', [ProfileController::class, 'getProfile'])->name('profile.get');
             Route::post('update-profile', [ProfileController::class, 'updateProfile'])->name('profile.update');
-            Route::post('location', [ProfileController::class, 'updateLocation'])->name('location.update');            
+            Route::post('location', [ProfileController::class, 'updateLocation'])->name('location.update');
 
             // Shipping Address CRUD
             Route::get('shipping-address', [ProfileShippingController::class, 'index'])->name('shipping-address.index');
             Route::post('shipping-address/show', [ProfileShippingController::class, 'show'])->name('shipping-address.show');
-            Route::post('shipping-address/store', [ProfileShippingController::class, 'store'])->name('shipping-address.store');            
+            Route::post('shipping-address/store', [ProfileShippingController::class, 'store'])->name('shipping-address.store');
             Route::post('shipping-address/delete', [ProfileShippingController::class, 'delete'])->name('shipping-address.delete');
 
             // Change Password
@@ -50,18 +49,18 @@ Route::group(['prefix' => 'v1', 'as' => 'api.'], function () {
             Route::post('delete-account', [ProfileController::class, 'deleteAccount'])->name('delete-account.delete');
         });
 
-        Route::middleware('auth:sanctum')->prefix('shop')->name('shop.')->group(function () {
+        Route::prefix('shop')->name('shop.')->group(function () {
             Route::get('get-shop', [ShopController::class, 'getShop'])->name('shop.get');
             Route::post('store-shop', [ShopController::class, 'storeShop'])->name('shop.store');
         });
 
-        Route::middleware('auth:sanctum')->prefix('product-category')->name('product-category.')->group(function () {
+        Route::prefix('product-category')->name('product-category.')->group(function () {
             Route::get('get-product-categories', [ProductCategoryController::class, 'getProductCategories'])->name('product-categories.get');
             Route::post('store-product-category', [ProductCategoryController::class, 'storeProductCategory'])->name('product-category.store');
             Route::post('delete-product-category', [ProductCategoryController::class, 'deleteProductCategory'])->name('product-category.delete');
         });
 
-        Route::middleware('auth:sanctum')->prefix('product-sub-category')->name('product-sub-category.')->group(function () {
+        Route::prefix('product-sub-category')->name('product-sub-category.')->group(function () {
             Route::get('get-product-sub-categories', [ProductSubCategoryController::class, 'getProductSubCategories'])->name('product-sub-categories.get');
             Route::post('store-product-sub-category', [ProductSubCategoryController::class, 'storeProductSubCategory'])->name('product-sub-category.store');
             Route::post('delete-product-sub-category', [ProductSubCategoryController::class, 'deleteProductSubCategory'])->name('product-sub-category.delete');
