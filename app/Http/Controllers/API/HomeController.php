5ec9fefe2da021faecbc9041b126bd405401a0f8
Febry San <97709170+f3brysan@users.noreply.github.com>
2026-03-02 22:22:05 +0700

    Enhance HomeController with improved product category and subcategory retrieval
    - Updated the `getProductCategories` method to fetch product categories along with their children, ensuring a more comprehensive response.
    - Refined the `getProductSubCategories` method to return subcategories for a given category slug, with improved error handling and response structure.
    - Added new `getProductsFilter` and `getProduct` methods with detailed documentation for filtering products and retrieving a product by its slug, respectively.
    - Enhanced API documentation with body and path parameter annotations for better clarity and usability.

--- app/Http/Controllers/API/HomeController.php
+++ app/Http/Controllers/API/HomeController.php
@@ -2,22 +2,27 @@
 
 namespace App\Http\Controllers\API;
 
-use App\Models\Product;
-use Illuminate\Http\Request;
-use App\Models\ProductCategory;
-use App\Models\ProductSubCategory;
 use App\Http\Controllers\Controller;
+use App\Models\Product;
+use App\Models\ProductCategory;
+use Dedoc\Scramble\Attributes\BodyParameter;
+use Dedoc\Scramble\Attributes\PathParameter;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 
 class HomeController extends Controller
 {   
     /**
+     * Get all product categories
+     * 
+     * This endpoint is used to get all product categories.
+     * @return \Illuminate\Http\JsonResponse
      * @unauthenticated
-     */
+     */    
     public function getProductCategories()
     {
         try {
-            $productCategories = ProductCategory::all();
+            $productCategories = ProductCategory::with('children')->whereHas('children')->get();
             return response()->json([
                 'status' => 'success',
                 'message' => 'Product categories fetched successfully',
@@ -33,24 +38,30 @@ class HomeController extends Controller
     }
 
     /**
+     * Get all product sub categories
+     * 
+     * This endpoint is used to get all product sub categories for a given product category slug.
+     *
+     * @param string $productCategorySlug
+     * @return \Illuminate\Http\JsonResponse
      * @unauthenticated
      */
+    #[PathParameter('productCategorySlug', description: 'The slug of the product category.', type: 'string', example: 'category-slug')]
     public function getProductSubCategories($productCategorySlug)
     {
         try {
-            $productCategory = ProductCategory::where('slug', $productCategorySlug)->first();
-            if (! $productCategory) {
+            $productSubCategories = ProductCategory::where('slug', $productCategorySlug)->with('children')->first();
+            if (! $productSubCategories) {
                 return response()->json([
                     'status' => 'error',
-                    'message' => 'Product category not found',
+                    'message' => 'Product sub categories not found',
                     'data' => []
                 ], 404);
             }
-            $productSubCategories = $productCategory->productSubCategories;
             return response()->json([
                 'status' => 'success',
                 'message' => 'Product sub categories fetched successfully',
-                'data' => $productSubCategories
+                'data' => $productSubCategories->children
             ]);
         } catch (\Throwable $th) {
             return response()->json([
@@ -62,8 +73,29 @@ class HomeController extends Controller
     }
 
     /**
+     * Get all products filter
+     *
+     * This endpoint is used to get all products filter.
+     *
+     * @bodyParam price_sort string optional The price sort. Example: asc
+     * @bodyParam price_from numeric optional The price from. Example: 100
+     * @bodyParam price_to numeric optional The price to. Example: 1000
+     * @bodyParam most_recent_sort boolean optional The most recent sort. Example: true
+     * @bodyParam name_sort string optional The name sort. Example: asc
+     * @bodyParam category_slug string optional The category slug. Example: category-slug
+     * @bodyParam sub_category_slug string optional The sub category slug. Example: sub-category-slug
+     * @bodyParam keyword string optional The keyword. Example: keyword
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
      * @unauthenticated
      */
+    #[BodyParameter('price_sort', description: 'Price sort.', type: 'string', example: 'asc')]
+    #[BodyParameter('price_from', description: 'Price from.', type: 'numeric', example: 100)]
+    #[BodyParameter('price_to', description: 'Price to.', type: 'numeric', example: 1000)]
+    #[BodyParameter('most_recent_sort', description: 'Most recent sort.', type: 'boolean', example: true)]
+    #[BodyParameter('name_sort', description: 'Name sort.', type: 'string', example: 'asc')]
+    #[BodyParameter('category_slug', description: 'Category slug.', type: 'string', example: 'category-slug')]
+    #[BodyParameter('keyword', description: 'Keyword.', type: 'string', example: 'keyword')]
     public function getProductsFilter(Request $request)
     {
         try {
@@ -73,8 +105,7 @@ class HomeController extends Controller
                 'price_to' => 'numeric',
                 'most_recent_sort' => 'boolean',
                 'name_sort' => 'string|in:asc,desc',
-                'category_slug' => 'string|max:255|exists:product_categories,slug',
-                'sub_category_slug' => 'string|max:255|exists:product_sub_categories,slug',
+                'category_slug' => 'string|max:255|exists:product_categories,slug',                
                 'keyword' => 'string|max:255',
                 'limit' => 'numeric',
             ]);
@@ -87,7 +118,7 @@ class HomeController extends Controller
                 ], 422);
             }
 
-            $products = Product::with('images', 'category', 'subCategory');
+            $products = Product::with('images', 'category', 'variants');
 
             if ($request->has('category_slug')) {
                 $productCategory = ProductCategory::where('slug', $request->category_slug)->first();
@@ -99,17 +130,6 @@ class HomeController extends Controller
                     ], 404);
                 }
                 $products->where('product_category_id', $productCategory->id);
-            }
-            if ($request->has('sub_category_slug')) {
-                $productSubCategory = ProductSubCategory::where('slug', $request->sub_category_slug)->first();
-                if (! $productSubCategory) {
-                    return response()->json([
-                        'status' => 'error',
-                        'message' => 'Product sub category not found',
-                        'data' => []
-                    ], 404);
-                }
-                $products->where('product_sub_category_id', $productSubCategory->id);
             }
 
             if ($request->has('price_sort')) {
@@ -151,12 +171,19 @@ class HomeController extends Controller
     }
 
     /**
+     * Get a product
+     *
+     * This endpoint is used to get a product by its slug.
+     *
+     * @param string $productSlug
+     * @return \Illuminate\Http\JsonResponse
      * @unauthenticated
      */
+    #[PathParameter('productSlug', description: 'The slug of the product.', type: 'string', example: 'product-slug')]
     public function getProduct($productSlug)
     {
         try {
-            $product = Product::with('images', 'category', 'subCategory')->where('slug', $productSlug)->first();
+            $product = Product::with('images', 'category', 'variants')->where('slug', $productSlug)->first();
             if (! $product) {
                 return response()->json([
                     'status' => 'error',
